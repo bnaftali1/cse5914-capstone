47bf9b4702a62b873713c23d7a3684bbbc4b9c02
Benny Naftali <naftali.1@buckeyemail.osu.edu>
2023-01-19 21:23:32 -0500

    added a search bar and configured input validation.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./style.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import React, { useState } from "react";
+import axios from "axios";
+function DisplayModal(props) {
+  const { name } = props;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Query Result:
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <pre>{JSON.stringify(name[0], null, 2)}</pre>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="outline-success" onClick={props.onHide}>
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+function App() {
+  const [modalShow, setModalShow] = useState(false);
+  //TODO: change state hooks to match the elastic search topic.
+  const [name, setName] = useState("");
+  const [studentInfo, setStudentInfo] = useState([]);
+
+  //TODO: replace axios API with the elastic search queries.
+  const client = axios.create({
+    baseURL: "https://directory.osu.edu",
+  });
+
+  //TODO: change submit handler to parse the elastic search query.
+  const handleSubmit = (e) => {
+    e.preventDefault(); //don't refresh page when submitted
+    if (name !== "") {
+      client.get("/fpjson.php?name_n=" + name).then((response) => {
+        setStudentInfo(response.data);
+      });
+      setName("");
+    }
+  };
+
+  return (
+    <>
+      <div className="App">
+        <div className="Wrapper">
+          <h3 className="SearchBarTitle">ElasticMedia</h3>
+          <Form onSubmit={handleSubmit} className="search-bar">
+            <InputGroup className="mb-3" size="lg">
+              <Form.Control
+                placeholder="Search your media"
+                aria-label=""
+                aria-describedby="basic-addon2"
+                onChange={(e) => {
+                  if (e.target.value.match(/^[A-Za-z0-9]*$/)) {
+                    setName(e.target.value);
+                  }
+                }}
+                value={name}
+              />
+              <Button
+                variant="outline-success"
+                type="submit"
+                id="button-addon2"
+                onClick={() => setModalShow(true)}
+              >
+                Search
+              </Button>
+            </InputGroup>
+          </Form>
+          <DisplayModal
+            show={modalShow}
+            name={studentInfo}
+            onHide={() => {
+              setModalShow(false);
+              setStudentInfo([]);
+            }}
+          />
+        </div>
+      </div>
+    </>
   );
 }
 
