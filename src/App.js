c2bd3d1cbd3d355e594ee0b7d64932881efc123d
Griffin Lavy <lavy.24@buckeyemail.osu.edu>
2023-01-24 18:39:26 -0500

    changed output to table and removed api call for now

--- src/App.js
+++ src/App.js
@@ -1,57 +1,48 @@
 import "./style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
+import Table from 'react-bootstrap/Table';
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import React, { useState } from "react";
-import axios from "axios";
-function DisplayModal(props) {
-  const { name } = props;
-  return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Query Result:
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <pre>{JSON.stringify(name[0], null, 2)}</pre>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="outline-success" onClick={props.onHide}>
-          Close
-        </Button>
-      </Modal.Footer>
-    </Modal>
-  );
-}
-function App() {
-  const [modalShow, setModalShow] = useState(false);
-  //TODO: change state hooks to match the elastic search topic.
-  const [name, setName] = useState("");
-  const [studentInfo, setStudentInfo] = useState([]);
 
-  //TODO: replace axios API with the elastic search queries.
-  const client = axios.create({
-    baseURL: "https://directory.osu.edu",
-  });
+function App() {
+  const [table, setTable] = useState(<></>);
+  //TODO: change state hooks to match the elastic search topic.
+  const [query, setQuery] = useState("");
+  //const [results, setResults] = useState([]);
 
   //TODO: change submit handler to parse the elastic search query.
   const handleSubmit = (e) => {
-    e.preventDefault(); //don't refresh page when submitted
-    if (name !== "") {
-      client.get("/fpjson.php?name_n=" + name).then((response) => {
-        setStudentInfo(response.data);
-      });
-      setName("");
-    }
-  };
+      e.preventDefault(); //don't refresh page when submitted
+      setTable(
+          <Table striped bordered hover variant="dark">
+              <thead>
+                  <tr>
+                      <th>#</th>
+                      <th>Title</th>
+                      <th>Release Year</th>
+                      <th>Rating</th>
+                  </tr>
+              </thead>
+              <tbody>
+                  <tr>
+                      <td>1</td>
+                      <td>Minions</td>
+                      <td>2015</td>
+                      <td>10.0</td>
+                  </tr>
+                  <tr>
+                      <td>2</td>
+                      <td>Minions: The Rise of Gru</td>
+                      <td>2022</td>
+                      <td>9.9</td>
+                  </tr>
+              </tbody>
+          </Table>
+      );
+    };
+
 
   return (
     <>
@@ -66,29 +57,22 @@ function App() {
                 aria-describedby="basic-addon2"
                 onChange={(e) => {
                   if (e.target.value.match(/^[A-Za-z0-9]*$/)) {
-                    setName(e.target.value);
+                    setQuery(e.target.value);
                   }
                 }}
-                value={name}
+                value={query}
               />
               <Button
                 variant="outline-success"
                 type="submit"
                 id="button-addon2"
-                onClick={() => setModalShow(true)}
               >
                 Search
-              </Button>
+                </Button>
+                         
             </InputGroup>
-          </Form>
-          <DisplayModal
-            show={modalShow}
-            name={studentInfo}
-            onHide={() => {
-              setModalShow(false);
-              setStudentInfo([]);
-            }}
-          />
+                  </Form>
+           {table}
         </div>
       </div>
     </>
