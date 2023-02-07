2e8a56c8ae57b97cc44e86d754392bfec48ae2e8
Griffin Lavy <lavy.24@buckeyemail.osu.edu>
2023-02-07 18:15:21 -0500

    Added table for movie recommendations that generates upon submission

--- src/App.js
+++ src/App.js
@@ -9,17 +9,30 @@ import React, { useEffect, useState } from "react";
 import testData from './testdata.json';
 
 function App() {
-  const [table, setTable] = useState(<></>);
+    const [table, setTable] = useState(<></>);
+    const savedTable = [];
   //TODO: change state hooks to match the elastic search topic.
   const [query, setQuery] = useState("");
   const [results, setResults] = useState([]);
-
+  const [savedValues, setSavedResults] = useState(new Map());
   //TODO: change submit handler to parse the elastic search query.
   const handleSubmit = (e) => {
       e.preventDefault(); //don't refresh page when submitted
       handleResults()
     };
-
+    const testFunction = (filmTitle, filmYear, filmRating,filmID) => {
+        var filmInfo = [filmTitle, filmYear, filmRating ]
+        if ( !savedValues.has(filmID)) {
+            const updateMap = (key, value) => {
+               setSavedResults(map => new Map(map.set(key, value)));
+               
+            }
+            updateMap(filmID, filmInfo);
+        }
+       
+        
+        
+    }
   useEffect(()=>{
     if (results.length !== 0){
     setTable(
@@ -43,9 +56,10 @@ function App() {
                 <td>{film.rating}</td>
                 <td className="d-grid gap-2" style={{"text-align":"center"}}>
                   <Button 
-                    variant="secondary"
-                    id="button-addon2"
-                    size = "sm">
+                              variant="secondary"
+                              id="button-addon2"
+                              size="sm"
+                              onClick={() =>testFunction(film.title, film.year,film.rating, film.id)}>
                   Click to save!
                   </Button>
                 </td>
@@ -79,9 +93,30 @@ function App() {
 
 
   return (
-    <>
+      <>
       <div className="App">
-        <div className="Wrapper">
+              <div className="Wrapper">
+                  {savedValues.size > 0 && (
+                      <Table striped bordered hover variant="dark">
+                          <thead>
+                              <tr>
+                                  <th>Title</th>
+                                  <th>Release Year</th>
+                                  <th>Rating</th>
+                              </tr>
+                          </thead>
+                          <tbody>
+                              {
+                                  savedValues.forEach((value) => {
+                                      savedTable.push(<tr><td>{value[0]}</td><td>{value[1]}</td><td>{value[2]}</td></tr>)
+                                  })
+                                  
+                                  
+                              }
+                              {savedTable}
+                          </tbody>
+                      </Table>)}
+
           <h3 className="SearchBarTitle">ElasticMedia</h3>
           <Form onSubmit={handleSubmit} className="search-bar">
             <InputGroup className="mb-3" size="lg">
@@ -106,7 +141,9 @@ function App() {
                          
             </InputGroup>
                   </Form>
-           {table}
+                  {table}
+                  
+                  
         </div>
       </div>
     </>
